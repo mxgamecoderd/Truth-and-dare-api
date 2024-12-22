93f4e8989fc27f4b98b59798c7639a400235a2a5
mxgamecoderd <damilaraolamilekan574@gmail.com>
2024-12-22 13:24:43 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "lqEFQbgL#8LD-Ea5HfzdwzVfv7s_Ya0TiffN-r7cJ_mk98EIVFgY",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/YFtmhOw.jpeg",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hello 👋 i am MX-GΔMΞCØDΞR, I am alive!!!",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+};
